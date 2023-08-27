75fb73a537fa92954ffb59b816a83533a513cc5d
Onkar Vallal <okvallal200@gmail.com>
2023-08-28 00:38:04 +0530

    commited

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,5 +1,12 @@
 // import Header from "./components/Header";
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from "./components/NavBar";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+import './App.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 
 function App() {
