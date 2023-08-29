2fe979654091ab1af871ad7770eb1d95126b0749
Abhishek <abhishekraut695@gmail.com>
2023-08-29 10:51:49 +0530

    commited

---
+++ Backend/src/main/java/com/woodworks/project/service/AdminService.java
@@ -0,0 +1,17 @@
+package com.woodworks.project.service;
+
+import com.woodworks.project.entities.Admin;
+
+public interface AdminService {
+
+	Admin validate(String email, String password);
+
+	void updateAdmin(Admin admin);
+
+	Admin findByEmail(String email);
+
+	void resetPassword(Admin admin, String password);
+
+	void deleteAdminById(Integer id);
+
+}
